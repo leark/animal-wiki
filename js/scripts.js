3b4824df5d29d4900561805f06d4ec1b21e39b4c
Seung Lee <shl7@uw.edu>
2022-06-02 15:19:40 -0700

    Hide other descriptions when one description opens
    Co-authored-by: Claire Thorington <claire.thorington@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
 $(document).ready(function () {
   $('.clickable').click(function () {
+    let animalDesc = $('.hidden');
+    if (animalDesc[0].style.display === 'block') {
+      animalDesc[0].style.display = 'none';
+    }
+    if (animalDesc[1].style.display === 'block') {
+      animalDesc[1].style.display = 'none';
+    }
+    if (animalDesc[2].style.display === 'block') {
+      animalDesc[2].style.display = 'none';
+    }
     $(this).siblings('.hidden').toggle();
   });
 });
