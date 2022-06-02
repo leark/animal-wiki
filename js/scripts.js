2a388a6fd1c58ebb6c6dac41e030470e334fdf42
Seung Lee <shl7@uw.edu>
2022-06-02 15:33:10 -0700

    Fix bug where other descriptions were not hiding

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,22 @@
 $(document).ready(function () {
   $('.clickable').click(function () {
+    let thisOff = $(this).siblings('.hidden').css('display') === 'none';
+
     let animalDesc = $('.hidden');
     if (animalDesc[0].style.display === 'block') {
-      animalDesc[0].style.display = 'none';
+      // .hide() works on jQuery element but not on regular DOM
+      $(animalDesc[0]).hide();
     }
     if (animalDesc[1].style.display === 'block') {
-      animalDesc[1].style.display = 'none';
+      $(animalDesc[1]).hide();
     }
     if (animalDesc[2].style.display === 'block') {
-      animalDesc[2].style.display = 'none';
+      $(animalDesc[2]).hide();
     }
-    $(this).siblings('.hidden').toggle();
+    if (thisOff) {
+      $(this).siblings('.hidden').toggle();
+    } else {
+      $(this).siblings('.hidden').hide();
+    }
   });
 });
